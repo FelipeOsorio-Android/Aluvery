e4263b2fb434a8822ac5c84acba124ed9be0e03a
FelipeOsorio-Android <feeh_devandroid@hotmail.com>
2023-10-12 16:51:26 -0300

    Create ProductItem

--- app/src/main/java/devandroid/felipe/aluvery/MainActivity.kt
+++ app/src/main/java/devandroid/felipe/aluvery/MainActivity.kt
@@ -3,13 +3,10 @@ package devandroid.felipe.aluvery
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import devandroid.felipe.aluvery.ui.components.ProductItem
 import devandroid.felipe.aluvery.ui.theme.AluveryTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +14,22 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AluveryTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                Surface {
+                    ProductItem()
                 }
             }
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
+fun MainPreview() {
     AluveryTheme {
-        Greeting("Android")
+        Surface {
+            ProductItem()
+        }
     }
 }
+
+
