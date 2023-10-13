f6f20785c2be90aa46707d3f8ccc4cc5538e6f0a
FelipeOsorio-Android <feeh_devandroid@hotmail.com>
2023-10-13 01:32:33 -0300

    final productItem

--- app/src/main/java/devandroid/felipe/aluvery/ui/components/DesafioProductItem.kt
+++ app/src/main/java/devandroid/felipe/aluvery/ui/components/DesafioProductItem.kt
@@ -2,86 +2,82 @@ package devandroid.felipe.aluvery.ui.components
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment.Companion.BottomCenter
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import devandroid.felipe.aluvery.R
 import devandroid.felipe.aluvery.ui.theme.Purple500
 import devandroid.felipe.aluvery.ui.theme.Teal200
 
 @Composable
-fun ProductItem() {
+fun DesafioProductItem() {
     Surface(
-        Modifier.padding(8.dp),
-        shape = RoundedCornerShape(15.dp),
+        Modifier
+            .padding(8.dp)
+            .clip(shape = RoundedCornerShape(15.dp)),
         shadowElevation = 4.dp
     ) {
-        Column(
+        Row(
             Modifier
-                .heightIn(250.dp, 300.dp)
-                .width(200.dp)
+                .height(200.dp)
+                .widthIn(250.dp, 300.dp)
         ) {
-            val imageSize = 100.dp
             Box(
                 modifier = Modifier
-                    .height(imageSize)
                     .background(
-                        brush = Brush.horizontalGradient(
-                            colors = listOf(
+                        brush = Brush.verticalGradient(
+                            listOf(
                                 Purple500, Teal200
                             )
                         )
                     )
-                    .fillMaxWidth()
+                    .fillMaxHeight()
+                    .width(100.dp)
             ) {
                 Image(
                     painter = painterResource(id = R.drawable.ic_launcher_background),
                     contentDescription = null,
                     Modifier
-                        .size(imageSize)
-                        .offset(y = imageSize / 2)
+                        .size(100.dp)
+                        .align(alignment = Alignment.Center)
+                        .offset(x = 50.dp)
                         .clip(shape = CircleShape)
-                        .align(BottomCenter)
+                        .border(2.dp, color = Purple500, shape = CircleShape)
+
                 )
             }
-            Spacer(modifier = Modifier.height(imageSize / 2))
-            Column(Modifier.padding(16.dp)) {
-                Text(
-                    text = LoremIpsum(50).values.first(),
-                    fontSize = 18.sp,
-                    fontWeight = FontWeight(700),
-                    maxLines = 2,
-                    overflow = TextOverflow.Ellipsis
-                )
-                Text(
-                    text = "R$ 14,99",
-                    Modifier.padding(top = 8.dp),
-                    fontSize = 14.sp,
-                    fontWeight = FontWeight(400)
-                )
+            Spacer(modifier = Modifier.width(50.dp))
+            Column(
+                Modifier
+                    .padding(16.dp)
+                    .align(CenterVertically)
+            ) {
+                Text(text = LoremIpsum(50).values.first(), maxLines = 4, overflow = TextOverflow.Clip)
+                Text(text = LoremIpsum(50).values.first(), maxLines = 2, overflow = TextOverflow.Clip)
             }
         }
     }
@@ -89,6 +85,6 @@ fun ProductItem() {
 
 @Preview(showBackground = true)
 @Composable
-fun ProductItemPreview() {
-    ProductItem()
+fun DesafioProductItemPreview() {
+    DesafioProductItem()
 }
