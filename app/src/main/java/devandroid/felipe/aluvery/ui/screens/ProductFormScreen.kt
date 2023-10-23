64c0d76164bd6242f2d26b52ddbff65f82159c3a
FelipeOsorio-Android <feeh_devandroid@hotmail.com>
2023-10-22 23:17:11 -0300

    add validation inputs forms

--- app/src/main/java/devandroid/felipe/aluvery/ui/screens/ProductFormScreen.kt
+++ app/src/main/java/devandroid/felipe/aluvery/ui/screens/ProductFormScreen.kt
@@ -2,13 +2,19 @@ package devandroid.felipe.aluvery.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
@@ -19,12 +25,16 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import devandroid.felipe.aluvery.R
 import devandroid.felipe.aluvery.model.ProductModel
 import java.math.BigDecimal
@@ -33,15 +43,46 @@ import java.util.regex.Pattern
 @Composable
 fun ProductFormScreen(modifier: Modifier = Modifier) {
 
+    var textUrl by remember { mutableStateOf("") }
+    var name by remember { mutableStateOf("") }
+    var price by remember { mutableStateOf("") }
+    var description by remember { mutableStateOf("") }
+    val errorFieldPrice by remember(price) {
+        mutableStateOf(Pattern.matches("^[^,-]*\$", price))
+    }
+    val validatePrice by remember(price) {
+        mutableStateOf(Pattern.matches("^\\d{1,3}[+.]\\d{1,3}[+.]\\d{1,2}\$", price))
+    }
+
+    val buttonEnabled by remember(name, price) {
+        mutableStateOf(name.isNotBlank() && price.isNotBlank() && validatePrice)
+    }
+
     Column(
         modifier
             .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.spacedBy(16.dp)
+            .padding(horizontal = 16.dp)
+            .verticalScroll(rememberScrollState())
+            .padding(PaddingValues(vertical = 16.dp)),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
     ) {
         Text(text = "Criando o produto", fontSize = 28.sp, fontWeight = FontWeight(500))
 
-        var textUrl by remember { mutableStateOf("") }
+        when {
+            textUrl.isNotBlank() -> {
+                AsyncImage(
+                    model = textUrl,
+                    contentDescription = null,
+                    Modifier
+                        .fillMaxWidth()
+                        .height(200.dp),
+                    contentScale = ContentScale.Crop,
+                    placeholder = painterResource(id = R.drawable.placeholder),
+                    error = painterResource(id = R.drawable.placeholder)
+                )
+            }
+        }
+
         OutlinedTextField(
             value = textUrl,
             onValueChange = { textUrl = it },
@@ -50,7 +91,10 @@ fun ProductFormScreen(modifier: Modifier = Modifier) {
             label = { Text(text = "Url") },
             placeholder = { Text(text = "URL da imagem") },
             singleLine = true,
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Uri),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Uri,
+                imeAction = ImeAction.Next
+            ),
             leadingIcon = {
                 Icon(
                     painter = painterResource(id = R.drawable.ic_url),
@@ -58,52 +102,86 @@ fun ProductFormScreen(modifier: Modifier = Modifier) {
                 )
             },
             trailingIcon = {
-                Icon(imageVector = Icons.Default.Close, contentDescription = null)
+                if(textUrl.isNotBlank()) {
+                    IconButton(onClick = { textUrl = "" }) {
+                        Icon(imageVector = Icons.Default.Close, contentDescription = null)
+                    }
+                }
             }
         )
 
-        var name by remember { mutableStateOf("") }
-        OutlinedTextField(
-            value = name,
-            onValueChange = { name = it },
-            Modifier
-                .fillMaxWidth(),
-            label = { Text(text = "Nome") },
-            placeholder = { Text(text = "Nome do produto") },
-            singleLine = true,
-            leadingIcon = {
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_product_name),
-                    contentDescription = null
+        Column {
+            OutlinedTextField(
+                value = name,
+                onValueChange = { name = it },
+                Modifier
+                    .fillMaxWidth(),
+                label = { Text(text = "Nome") },
+                placeholder = { Text(text = "Nome do produto") },
+                singleLine = true,
+                leadingIcon = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_product_name),
+                        contentDescription = null
+                    )
+                },
+                trailingIcon = {
+                    if (name.isNotBlank()) {
+                        IconButton(onClick = { name = "" }) {
+                            Icon(imageVector = Icons.Default.Close, contentDescription = null)
+                        }
+                    }
+                },
+                keyboardOptions = KeyboardOptions(
+                    imeAction = ImeAction.Next,
+                    capitalization = KeyboardCapitalization.Words
                 )
-            },
-            trailingIcon = {
-                Icon(imageVector = Icons.Default.Close, contentDescription = null)
-            }
-        )
+            )
+            Text(text = "Campo Obrigatório*", style = MaterialTheme.typography.caption)
+        }
 
-        var price by remember { mutableStateOf("") }
-        OutlinedTextField(
-            value = price,
-            onValueChange = { price = it },
-            Modifier
-                .fillMaxWidth(),
-            label = { Text(text = "Preço") },
-            placeholder = { Text(text = "Preço do produto") },
-            singleLine = true,
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-            leadingIcon = {
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_price),
-                    contentDescription = null
+        Column {
+            OutlinedTextField(
+                value = price,
+                onValueChange = { price = it },
+                Modifier
+                    .fillMaxWidth(),
+                label = { Text(text = "Preço") },
+                placeholder = { Text(text = "Preço do produto") },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Decimal,
+                    imeAction = ImeAction.Next
+                ),
+                leadingIcon = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_price),
+                        contentDescription = null
+                    )
+                },
+                trailingIcon = {
+                    if (price.isNotBlank()) {
+                        IconButton(onClick = { price = "" }) {
+                            Icon(imageVector = Icons.Default.Close, contentDescription = null)
+                        }
+                    }
+                },
+                isError = !errorFieldPrice
+            )
+            if(!errorFieldPrice) {
+                Text(
+                    text = "Valor Invalido",
+                    color = MaterialTheme.colors.error,
+                    style = MaterialTheme.typography.caption
                 )
-            },
-            trailingIcon = {
-                Icon(imageVector = Icons.Default.Close, contentDescription = null)
+            } else {
+                Text(
+                    text = "Campo Obrigatório*",
+                    style = MaterialTheme.typography.caption
+                )
             }
-        )
+        }
 
-        var description by remember { mutableStateOf("") }
         OutlinedTextField(
             value = description,
             onValueChange = { description = it },
@@ -112,15 +190,11 @@ fun ProductFormScreen(modifier: Modifier = Modifier) {
                 .heightIn(min = 100.dp),
             label = { Text(text = "Descrição") },
             placeholder = { Text(text = "Descrição do produto") },
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Default,
+                capitalization = KeyboardCapitalization.Sentences
+            )
         )
-
-        val validatePrice by remember(price) {
-            mutableStateOf(Pattern.matches("^[0-9]+\$", price))
-        }
-
-        val buttonEnabled by remember(name, price) {
-            mutableStateOf(name.isNotBlank() && price.isNotBlank() && validatePrice)
-        }
 
         Button(
             onClick = {
